47c7abb8a772061c6640be713fe984661f44eff1
Abensett <bensettiaimen@mailo.com>
2022-08-01 03:27:39 +0200

    go

--- srcs/game_raycasting.c
+++ srcs/game_raycasting.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 09:28:37 by flee              #+#    #+#             */
-/*   Updated: 2022/08/01 03:27:00 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/01 03:27:32 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -135,9 +135,9 @@ void	draw_sky_2(t_game *game, int y)
 		game->ray.floor_x += game->ray.floor_step_x;
 		game->ray.floor_y += game->ray.floor_step_y;
 		my_mlx_pixel_put(&game->windows, x, y,
-			img_pix_get(&game->sky, game->ray.tex_x, game->ray.tex_y));
+			img_pix_get(&game->west, game->ray.tex_x, game->ray.tex_y));
 		my_mlx_pixel_put(&game->windows, x, game->ray.draw_start,
-			img_pix_get(&game->floor, game->ray.tex_x, game->ray.tex_y));
+			img_pix_get(&game->west, game->ray.tex_x, game->ray.tex_y));
 		x++;
 	}
 }
