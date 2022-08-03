19d6b2a7b67203feac7937319b0db91768583b21
Abensett <bensettiaimen@mailo.com>
2022-08-03 17:04:45 +0200

    go

--- srcs/game_raycasting.c
+++ srcs/game_raycasting.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 09:28:37 by flee              #+#    #+#             */
-/*   Updated: 2022/08/03 17:03:19 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/03 17:04:43 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -136,7 +136,8 @@ void	raycasting(t_game *game)
 		verline(game, x++, 0);
 	}
 	draw_mini_map(game, 0);
-	ft_draw_heart(game, 100);
+	mlx_put_image_to_window(game->mlx.mlx, game->mlx.windows,
+		game->gun.img, 0, 0);
 	mlx_put_image_to_window(game->mlx.mlx, game->mlx.windows,
 		game->windows.img, 0, 0);
 }
