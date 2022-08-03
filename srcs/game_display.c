552ba7d26d17c31e57ff1283d69f2c513c77ccc9
Abensett <bensettiaimen@mailo.com>
2022-08-03 15:52:04 +0200

    go

--- srcs/game_display.c
+++ srcs/game_display.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 09:28:50 by flee              #+#    #+#             */
-/*   Updated: 2022/08/03 15:51:04 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/03 15:52:03 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,10 +56,10 @@ void	verline(t_game *game, int x, int sky)
 	
 	x = 0;
 	while (x < 20)
-		my_mlx_pixel_put(&game->windows, WINDOWS_X / 2 - 10 + x++, WINDOWS_Y / 2, 0xFF0000)\
+		my_mlx_pixel_put(&game->windows, WINDOWS_X / 2 - 10 + x++, WINDOWS_Y / 2, 0xFF0000);
 	x = 0;
 	while (x < 20)
-		my_mlx_pixel_put(&game->windows, WINDOWS_X / 2 - 10, WINDOWS_Y / 2 + x++, 0xFF0000)\
+		my_mlx_pixel_put(&game->windows, WINDOWS_X / 2 - 10, WINDOWS_Y / 2 + x++, 0xFF0000);
 	x = 0;
 	while (game->ray.draw_start < game->ray.draw_end)
 	{
