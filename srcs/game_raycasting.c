5cac28e419d55350d0189e84874bb29b9d5c5d06
Abensett <bensettiaimen@mailo.com>
2022-08-04 19:23:23 +0200

    go

--- srcs/game_raycasting.c
+++ srcs/game_raycasting.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 09:28:37 by flee              #+#    #+#             */
-/*   Updated: 2022/08/04 16:29:31 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/04 19:23:07 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -137,6 +137,7 @@ void	raycasting(t_game *game)
 	}
 	draw_mini_map(game, 0);
 	ft_draw_heart(game, 5);
+	
 	mlx_put_image_to_window(game->mlx.mlx, game->mlx.windows,
 		game->windows.img, 0, 0);
 	mlx_put_image_to_window(game->mlx.mlx, game->mlx.windows,
