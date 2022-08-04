2c55ec736d1bc68f80fc9abd0dfd7d3fa35dd292
Abensett <bensettiaimen@mailo.com>
2022-08-04 19:36:16 +0200

    go

--- srcs/game_display.c
+++ srcs/game_display.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 09:28:50 by flee              #+#    #+#             */
-/*   Updated: 2022/08/04 19:17:41 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/04 19:36:14 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,8 @@ void	put_tex(t_game *game, int x)
 			img_pix_get(&game->north, game->ray.tex_x, game->ray.tex_y));
 	else if (game->ray.side == 0 && game->ray.map_x <= game->player.pos_x)
 		my_mlx_pixel_put(&game->windows, x, game->ray.draw_start,
-			img_pix_get(&game->south, game->ray.tex_x, game->ray.tex_y));
+		apply_shadow(game->ray.perp_wall_dist ,
+			img_pix_get(&game->south, game->ray.tex_x, game->ray.tex_y)));
 	else if (game->ray.side == 1 && game->ray.map_y <= game->player.pos_y)
 		my_mlx_pixel_put(&game->windows, x, game->ray.draw_start,
 			apply_shadow(game->ray.perp_wall_dist ,
