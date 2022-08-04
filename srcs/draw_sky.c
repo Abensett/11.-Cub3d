576f3e8a92adae4401dba52c8fabcb89e2c398e7
Abensett <bensettiaimen@mailo.com>
2022-08-04 19:20:28 +0200

    go

--- srcs/draw_sky.c
+++ srcs/draw_sky.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 10:42:00 by abensett          #+#    #+#             */
-/*   Updated: 2022/08/04 19:19:48 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/04 19:20:27 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		apply_shadow(double dist, int color)
 	int transparency;
 	int new_color;
 	
-	transparency = (int)fmin(((color >> 24) + 0.100) * (dist * 600), 255);
+	transparency = (int)fmin(((color >> 24) + 0.01) * (dist * 600), 255);
 	new_color = (transparency << 24 | color);
 	return (new_color);
 }
