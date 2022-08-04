f4b7b9611f32addd87f432272d6324a1c5757c97
Abensett <bensettiaimen@mailo.com>
2022-08-04 19:01:47 +0200

    go

--- includes/Cub3D.h
+++ includes/Cub3D.h
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 11:06:09 by flee              #+#    #+#             */
-/*   Updated: 2022/08/04 18:57:33 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/04 19:01:47 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -206,6 +206,5 @@ bool			check_move(t_game *game, float tmp_x, float tmp_y);
 
 void			draw_mini_map(t_game *game, int color);
 void			draw_sky(t_game *game, int y);
-void			apply_shadow(double dist, int *color);
 void			ft_draw_heart(t_game *game, int hp);
 #endif
