b67c35305048747003cb16c87ecdb09bd863a414
Abensett <bensettiaimen@mailo.com>
2022-08-04 19:13:56 +0200

    go

--- srcs/draw_sky.c
+++ srcs/draw_sky.c
@@ -6,21 +6,21 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 10:42:00 by abensett          #+#    #+#             */
-/*   Updated: 2022/08/04 19:08:36 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/04 19:13:50 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cub3D.h"
 
-int		apply_shadow(double dist, int *color)
+int		apply_shadow(double dist, int color)
 {
 	int transparency;
-
-	printf("%d 1\n", color);
-	transparency = (int)fmin(((*color >> 24) + 0.1) * (dist * 600), 255);
-	*color = (transparency << 24 | *color);
-	printf("%d 2\n", color);
-	return (*color);
+	int new_color;
+	
+	printf("dist: %f\n", dist);
+	transparency = (int)fmin(((color >> 24) + 0.1) * (dist * 600), 255);
+	new_color = (transparency << 24 | color);
+	return (new_color);
 }
 
 void	draw_sky_2(t_game *game, int y)
@@ -41,10 +41,10 @@ void	draw_sky_2(t_game *game, int y)
 		
 		my_mlx_pixel_put(&game->windows, x, y,
 			apply_shadow(game->ray.perp_wall_dist ,
-			&img_pix_get(&game->sky, game->ray.tex_x, game->ray.tex_y)));
+			img_pix_get(&game->sky, game->ray.tex_x, game->ray.tex_y)));
 		my_mlx_pixel_put(&game->windows, x, WINDOWS_Y - y - 1,
 		apply_shadow(game->ray.perp_wall_dist ,
-		 &img_pix_get(&game->floor, game->ray.tex_x,
+		 img_pix_get(&game->floor, game->ray.tex_x,
 			 game->ray.tex_y)));
 		x++;
 	}
