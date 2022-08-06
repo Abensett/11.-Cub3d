74a4c7ad0b9f4d9996340a3a19a0fd03aa943241
Abensett <bensettiaimen@mailo.com>
2022-08-06 22:37:29 +0200

    go

--- srcs/game_loop.c
+++ srcs/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 16:54:15 by flee              #+#    #+#             */
-/*   Updated: 2022/08/04 16:28:29 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/06 22:34:36 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void	game_loop(t_game *game)
 {
 	game->mlx.mlx = mlx_init();
 	game->mlx.windows = mlx_new_window(game->mlx.mlx,
-			WINDOWS_X, WINDOWS_Y, "Xub3d");
+			WINDOWS_X, WINDOWS_Y, "Cub3d");
 	open_img(game);
 	mlx_loop_hook(game->mlx.mlx, &render_next_frame, game);
 	mlx_hook(game->mlx.windows, 2, 1L << 0, input, game);
