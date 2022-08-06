16e507d3bf721e8102faaa2da5f6b8886e4a7fe0
Abensett <bensettiaimen@mailo.com>
2022-08-06 17:46:25 +0200

    go

--- srcs/life_bar.c
+++ srcs/life_bar.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 16:36:38 by abensett          #+#    #+#             */
-/*   Updated: 2022/08/06 17:42:38 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/06 17:46:24 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,9 @@ void	ft_life_bar(t_game *game)
 	while (j < WINDOWS_Y - 100)
 	{
 		i = WINDOWS_X - 200;
-		while (i < WINDOWS_X -100)
+		while (i < 100)
 		{
-			color = (i - WINDOWS_X - 200 < 5 * (100 / 100))
+			color = (i - WINDOWS_X - w < 5 * (100 / 100))
 				? 0x9F0000 : 0xFFFFFF;
 			my_mlx_pixel_put(&game->windows, i++, j, color);
 		}
