028bc7c8c9e7b409065de6a1c4be31fd90f92d48
Abensett <bensettiaimen@mailo.com>
2022-08-06 22:27:29 +0200

    go

--- srcs/life_bar.c
+++ srcs/life_bar.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 16:36:38 by abensett          #+#    #+#             */
-/*   Updated: 2022/08/06 22:27:19 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/06 22:27:28 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,7 +83,7 @@ void	ft_gun(t_game *game, int shooting)
 		j = 0.6 *  WINDOWS_Y;
 		while (j + game->gun_shift <  WINDOWS_Y)
 		{
-			color = get_tex_color(&game->gun,
+			color = get_tex_color(game->gun,
 				((i - 0.4 * WINDOWS_X) / (0.4 * WINDOWS_X)),
 				((j - 0.6 *  WINDOWS_Y) / (0.4 * WINDOWS_Y)), 1);
 			if (color != 0)
