646f0112842c2b6299548ceeec618f3fa502b5ff
Abensett <bensettiaimen@mailo.com>
2022-08-06 21:31:08 +0200

    go

--- includes/Cub3D.h
+++ includes/Cub3D.h
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 11:06:09 by flee              #+#    #+#             */
-/*   Updated: 2022/08/06 21:30:16 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/06 21:30:38 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@
 # include <math.h>
 # include "libft.h"
 # include "mlx.h"
-# include <stdbool.h>
 
 # define WINDOWS_X 	1500
 # define WINDOWS_Y 	1000
