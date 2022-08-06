2c454372a26bf1f1f95b62864dd18f28d9c4f245
Abensett <bensettiaimen@mailo.com>
2022-08-06 22:27:19 +0200

    go

--- srcs/life_bar.c
+++ srcs/life_bar.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 16:36:38 by abensett          #+#    #+#             */
-/*   Updated: 2022/08/06 22:26:59 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/06 22:27:19 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,7 @@ void	ft_gun(t_game *game, int shooting)
 		{
 			color = get_tex_color(&game->gun,
 				((i - 0.4 * WINDOWS_X) / (0.4 * WINDOWS_X)),
-				((j - 0.6 *  WINDOWS_Y) / (0.4 * WINDOWS_Y)));
+				((j - 0.6 *  WINDOWS_Y) / (0.4 * WINDOWS_Y)), 1);
 			if (color != 0)
 				my_mlx_pixel_put(&game->windows, i, j + game->gun_shift, color);
 			j++;
