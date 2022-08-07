703a11b44643020ae6118f050c5d1cbe11869509
Abensett <bensettiaimen@mailo.com>
2022-08-07 23:20:03 +0200

    go

--- srcs/game_move2.c
+++ srcs/game_move2.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 21:20:28 by abensett          #+#    #+#             */
-/*   Updated: 2022/08/07 23:19:37 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/07 23:20:02 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,10 +49,11 @@ int	input(int key, t_game *game)
 		rotate_right(game);
 	else if (key == 32)
 	{
-		// system("cvlc --play-and-exit img/gun_shot.mp3 &>/dev/null &");
+		system("cvlc --play-and-exit img/gun_shot.mp3 &>/dev/null &");
 		usleep(500);
 		game->shotornot = 1;
 	}
+	printf("Keypress: %d\n", key);
 	return (0);
 }
 
