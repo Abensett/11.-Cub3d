77bd1a2dfee04296f74952b8f05217d187c57c41
Abensett <bensettiaimen@mailo.com>
2022-08-08 00:40:16 +0200

    go

--- srcs/gameoverorwon.c
+++ srcs/gameoverorwon.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 00:05:30 by abensett          #+#    #+#             */
-/*   Updated: 2022/08/08 00:33:16 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/08 00:40:15 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ void	write_on_screen(t_game *game, char *text, int pos[2], int color)
 
 	x = pos[0] - ft_strlen(text) * 5;
 	y = pos[1];
-	mlx_string_put(game->mlx.mlx, game->mlx.windows, x, y, color, text);
+	mlx_string_put(game->mlx.mlx, game->mlx.windows, 0, 0, color, text);
 }
