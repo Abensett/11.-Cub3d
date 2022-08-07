8fb3a022638fd70d5aec51bf626ca9867950aa27
Abensett <bensettiaimen@mailo.com>
2022-08-07 20:36:18 +0200

    go

--- srcs/life_bar.c
+++ srcs/life_bar.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 16:36:38 by abensett          #+#    #+#             */
-/*   Updated: 2022/08/07 20:36:05 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/07 20:36:17 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,7 +94,7 @@ void	ft_gun(t_game *game)
 	game->gun_shift = game->gun_shift + (game->gun_dir ? -5 : 5);
 	if (game->gun_shift < 0)
 		game->gun_dir = 0;
-	else if (game->gun_shift > 0.02 * WINDOWS_Y)
+	else if (game->gun_shift > 0.05 * WINDOWS_Y)
 		game->gun_dir = 1;
 	if(game->shotornot == 1)
 	{
