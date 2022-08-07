9da84961f6f3062a5df9ce9d292cee04bc3ad9e5
Abensett <bensettiaimen@mailo.com>
2022-08-07 23:21:33 +0200

    go

--- srcs/game_move2.c
+++ srcs/game_move2.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 21:20:28 by abensett          #+#    #+#             */
-/*   Updated: 2022/08/07 23:21:21 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/07 23:21:32 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ int	input(int key, t_game *game)
 		game->shotornot = 1;
 	}
 	if (key == 65505)
-		game->speed = 0.3;
+		game->speed = 0.15;
 
 	printf("Keypress: %d\n", key);
 	return (0);
