6a21b6c34a879de6ec5ea6c5c57f0de64395d9b5
Abensett <bensettiaimen@mailo.com>
2022-08-07 20:13:00 +0200

    go

--- srcs/game_loop.c
+++ srcs/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 16:54:15 by flee              #+#    #+#             */
-/*   Updated: 2022/08/07 20:12:49 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/07 20:13:00 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ int	color_get(t_img *img, int x, int y)
 
 int	ft_handle_shoot(int button, int x, int y, t_game *game)
 {
-	int pid;
 	if (button == 1 && game->shotornot == 0)
 	{
 	 	game->shotornot = 1;
