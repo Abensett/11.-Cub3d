f60d811cdc80ea8818a13802f1000ca8daed8f9e
Abensett <bensettiaimen@mailo.com>
2022-08-08 10:06:07 +0200

    go

--- srcs/game_raycasting.c
+++ srcs/game_raycasting.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 09:28:37 by flee              #+#    #+#             */
-/*   Updated: 2022/08/08 10:05:48 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/08 10:06:06 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -144,6 +144,6 @@ void	raycasting(t_game *game)
 		mlx_string_put(game->mlx.mlx, game->mlx.windows,  WINDOWS_X - 375, 70,
 		 0xFF00000, "You're about to die !");
 	if(game->bullets <= 0)
-			mlx_string_put(game->mlx.mlx, game->mlx.windows,  WINDOWS_X - 700, 70,
+			mlx_string_put(game->mlx.mlx, game->mlx.windows,  WINDOWS_X - 800, 70,
 		 0xFFFFFF, "No more bullets !");
 }
