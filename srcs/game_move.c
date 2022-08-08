70e0d0e368f0c4fdbd4216597ca20e299801177d
Abensett <bensettiaimen@mailo.com>
2022-08-08 10:10:02 +0200

    go

--- srcs/game_move.c
+++ srcs/game_move.c
@@ -47,8 +47,8 @@ void	move_right(t_game *game)
 	else
 	{
 		game->life -= 5;
-		game->player.pos_x -= game->player.dir_y * game->speed;
-		game->player.pos_y += game->player.dir_x * game->speed;
+		game->player.pos_x -= game->player.dir_y * game->speed * 4;
+		game->player.pos_y += game->player.dir_x * game->speed * 4;
 	}
 }
 
