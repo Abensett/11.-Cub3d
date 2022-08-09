b2cff1b4e761c3f99bca179d76c8f924095bd682
Abensett <bensettiaimen@mailo.com>
2022-08-09 23:59:48 +0200

    go

--- srcs/game_raycasting.c
+++ srcs/game_raycasting.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 09:28:37 by flee              #+#    #+#             */
-/*   Updated: 2022/08/09 23:58:49 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/09 23:59:47 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -140,7 +140,7 @@ void	raycasting(t_game *game)
 	draw_mini_map(game, 0);
 	ft_life_bar(game);
 	ft_gun(game);
-	// draw_sprites(game);
+	draw_sprites(game);
 	check_dead_win(game);
 	infos(game);
 }
