bc1385ad14c09a89d89dd8e2f0803695e9ca7b1f
Abensett <bensettiaimen@mailo.com>
2022-08-10 00:39:14 +0200

    go

--- srcs/sprites.c
+++ srcs/sprites.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 01:58:17 by abensett          #+#    #+#             */
-/*   Updated: 2022/08/10 00:38:59 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/10 00:39:13 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -170,7 +170,7 @@ void	draw_sprites(t_game *game)
 		j = 0.6 * WINDOWS_Y;
 		while (j < WINDOWS_Y)
 		{
-			color = my_tex_color(game->gun,
+			color = my_tex_color(game->gun[0],
 					((i - 0.4 * WINDOWS_X) / (0.4 * WINDOWS_X)),
 					((j - 0.6 * WINDOWS_Y) / (0.4 * WINDOWS_Y)), 1);
 			if (color != 0)
