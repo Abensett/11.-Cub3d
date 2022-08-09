cc7e8b2fc53cbceeb5f809588b85e4e77068fe87
Abensett <bensettiaimen@mailo.com>
2022-08-10 00:16:52 +0200

    go

--- srcs/game_raycasting.c
+++ srcs/game_raycasting.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 09:28:37 by flee              #+#    #+#             */
-/*   Updated: 2022/08/10 00:09:21 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/10 00:16:51 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -136,7 +136,6 @@ void	raycasting(t_game *game)
 		verline(game, x++, 0);
 		
 		game->depth[x] = game->ray.perp_wall_dist;
-		printf("%f\n", game->depth[x]);
 	}
 	draw_mini_map(game, 0);
 	ft_life_bar(game);
