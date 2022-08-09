31bc061fe4f2114bf4ece43fd1a6e60a7d71aa86
Abensett <bensettiaimen@mailo.com>
2022-08-09 02:38:47 +0200

    go

--- includes/Cub3D.h
+++ includes/Cub3D.h
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 11:06:09 by flee              #+#    #+#             */
-/*   Updated: 2022/08/09 02:31:00 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/09 02:38:46 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -151,6 +151,29 @@ typedef struct s_img
 	int		height;
 }	t_img;
 
+struct Sprite
+{
+  double x;
+  double y;
+  int texture;
+};
+
+typedef struct		s_sprite
+{
+	int		pos[2];
+	double	distance;
+	int		killable;
+	int		destroyed;
+	t_img	texture;
+}					t_sprite;
+
+typedef struct		s_spritedata
+{
+	double	transform[2];
+	int		sprite_x;
+	int		sprite_size;
+	int		index;
+}					t_spritedata;
 typedef struct s_game
 {
 	int			won;
@@ -180,29 +203,7 @@ typedef struct s_game
 	double			*depth;
 }	t_game;
 
-struct Sprite
-{
-  double x;
-  double y;
-  int texture;
-};
 
-typedef struct		s_sprite
-{
-	int		pos[2];
-	double	distance;
-	int		killable;
-	int		destroyed;
-	t_img	texture;
-}					t_sprite;
-
-typedef struct		s_spritedata
-{
-	double	transform[2];
-	int		sprite_x;
-	int		sprite_size;
-	int		index;
-}					t_spritedata;
 
 void			get_file(char *map, t_game *game);
 void			parse_map(t_game *game);
