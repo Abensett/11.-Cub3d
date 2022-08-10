75a03b415b8cd0b03da4875242043a1fd50e0187
Abensett <bensettiaimen@mailo.com>
2022-08-10 03:42:59 +0200

    go

--- srcs/game_loop.c
+++ srcs/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 16:54:15 by flee              #+#    #+#             */
-/*   Updated: 2022/08/10 03:41:42 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/10 03:42:59 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ void	ft_handle_shoot(t_game *game)
 	{
 		system("cvlc --play-and-exit img/gun_shot2.mp3 &>/dev/null &");
 		game->bullets--;
+		game->shotornot++;
 		i = game->nb_sprites - 1;
 		while (i >= 0)
 		{
