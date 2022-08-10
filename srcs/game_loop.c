f2a39714fa1405490be0f960db1f2eba65f221e2
Abensett <bensettiaimen@mailo.com>
2022-08-10 03:57:59 +0200

    go

--- srcs/game_loop.c
+++ srcs/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 16:54:15 by flee              #+#    #+#             */
-/*   Updated: 2022/08/10 03:57:47 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/10 03:57:59 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	render_next_frame(t_game *game)
 {
 	if (game->life <= 0)
 	{
-		sleep(2);
+		sleep(5);
 		free_and_destroy(game);
 	}
 	raycasting(game);
