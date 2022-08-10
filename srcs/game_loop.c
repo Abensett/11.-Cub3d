1a0dc2d3699a103c4e78af0de3d2f9ea0e94102b
Abensett <bensettiaimen@mailo.com>
2022-08-10 03:18:29 +0200

    go

--- srcs/game_loop.c
+++ srcs/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 16:54:15 by flee              #+#    #+#             */
-/*   Updated: 2022/08/10 03:15:07 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/10 03:18:13 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ int	color_get(t_img *img, int x, int y)
 
 void	ft_handle_shoot(t_game *game)
 {
-	int		i;
 	if (game->bullets >= 0 && game->shotornot < 2) 
 	{
 		system("cvlc --play-and-exit img/gun_shot2.mp3 &>/dev/null &");
@@ -38,17 +37,18 @@ void	ft_handle_shoot(t_game *game)
 	}
 	else
 		system("cvlc --play-and-exit img/knife.mp3 &>/dev/null &");
-	i = game->nb_sprites - 1;
-		while (i > 0)
+	i = game->nb_sprites;
+
+	while (i > 0)
+	{
+		if (game->sprites[i].alive)
 		{
-			if (game->sprites[i].alive)
-			{
-				game->map.map[game->sprites[i].pos[0]][game->sprites[i].pos[1]] = '0';
-				game->sprites[i].dead = 1;
-				break ;
-			}
-			i--;
+			game->map.map[game->sprites[i].pos[0]][game->sprites[i].pos[1]] = '0';
+			game->sprites[i].dead = 1;
+			break ;
 		}
+		i--;
+	}
 	game->shotornot++;
 }
 
