f09d26ca8e95de58bfd56a0de695691307e36fed
Abensett <bensettiaimen@mailo.com>
2022-08-10 02:02:58 +0200

    go

--- srcs/gameoverorwon.c
+++ srcs/gameoverorwon.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 00:05:30 by abensett          #+#    #+#             */
-/*   Updated: 2022/08/10 01:58:16 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/10 01:58:45 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,12 @@ void 	check_dead_win(t_game *game)
 {
 	load_texture(game, &game->game_over, "./img/gameover.xpm");
 	if(game->life <= 0)
+	{
 			mlx_put_image_to_window(game->mlx.mlx, game->mlx.windows,
 		game->game_over.img, 0, -150);
+	}
 	else
 		mlx_put_image_to_window(game->mlx.mlx, game->mlx.windows,
 		game->windows.img, 0, 0);
-	system("killall -9 vlc");
 	
 }
