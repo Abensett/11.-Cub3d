ef3d03dda204b7554e25f606800880386afc9a75
Abensett <bensettiaimen@mailo.com>
2022-08-10 03:35:27 +0200

    go

--- srcs/game_loop.c
+++ srcs/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 16:54:15 by flee              #+#    #+#             */
-/*   Updated: 2022/08/10 03:19:54 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/10 03:35:25 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,12 @@ void	ft_handle_shoot(t_game *game)
 	{
 		system("cvlc --play-and-exit img/gun_shot2.mp3 &>/dev/null &");
 		game->bullets--;
-	}
-	else
-		system("cvlc --play-and-exit img/knife.mp3 &>/dev/null &");
-	i = game->nb_sprites - 1;
-	while (i >= 0)
-	{
-		if (game->sprites[i].alive)
+		game->shotornot++;
+		i = game->nb_sprites - 1;
+		while (i >= 0)
 		{
+			if (game->sprites[i].alive)
+			{
 			game->map.map[game->sprites[i].pos[0]][game->sprites[i].pos[1]] = '0';
 			game->sprites[i].dead = 1;
 			if (i == 0)
@@ -50,7 +48,10 @@ void	ft_handle_shoot(t_game *game)
 			break ;
 		}
 		i--;
+		return ;
 	}
+	else
+		system("cvlc --play-and-exit img/knife.mp3 &>/dev/null &");
 	game->shotornot++;
 }
 
