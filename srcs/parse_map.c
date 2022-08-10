fc0618f0ae763dcdbdcc51480e8d27e8e7dcb9f0
Abensett <bensettiaimen@mailo.com>
2022-08-10 09:30:27 +0200

    Done

--- srcs/parse_map.c
+++ srcs/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 15:42:07 by flee              #+#    #+#             */
-/*   Updated: 2022/08/10 09:29:54 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/10 09:30:27 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -134,7 +134,7 @@ void	parse_map(t_game *game)
 	}
 	game->texture.skys = ft_strdup("./img/sky.xpm");
 	game->texture.floors = ft_strdup("./img/floor.xpm");
-	game->depth = malloc(WINDOWS_X, sizeof(double));
+	game->depth = ft_calloc(WINDOWS_X, sizeof(double));
 	if (game->depth == 0)
 		exit(1);
 }
