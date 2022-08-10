45694fcd755d1b7e918a886c1083e82f6d1d03e7
Abensett <bensettiaimen@mailo.com>
2022-08-10 03:45:11 +0200

    go

--- srcs/game_move2.c
+++ srcs/game_move2.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 21:20:28 by abensett          #+#    #+#             */
-/*   Updated: 2022/08/08 11:38:28 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/10 03:45:10 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,8 @@ int	input(int key, t_game *game)
 		rotate_left(game);
 	else if (key == 65363)
 		rotate_right(game);
-	else if (key == 32)
-		ft_handle_shoot(game);
+	// else if (key == 32)
+	// 	ft_handle_shoot(game);
 	if (key == 65505)
 		game->speed = 0.06;
 	return (0);
