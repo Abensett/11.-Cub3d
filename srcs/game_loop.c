d2370b64c783d79073cb2ee9aa949d3ab70bb14e
Abensett <bensettiaimen@mailo.com>
2022-08-10 03:19:38 +0200

    go

--- srcs/game_loop.c
+++ srcs/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: abensett <abensett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 16:54:15 by flee              #+#    #+#             */
-/*   Updated: 2022/08/10 03:19:25 by abensett         ###   ########.fr       */
+/*   Updated: 2022/08/10 03:19:37 by abensett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ void	ft_handle_shoot(t_game *game)
 		{
 			game->map.map[game->sprites[i].pos[0]][game->sprites[i].pos[1]] = '0';
 			game->sprites[i].dead = 1;
-			if (i < 0)
+			if (i == 0)
 				game->won = 1;
 			break ;
 		}
